729fdb1f96454884a17181774b39c30f72c448c7
Steve Losh <steve@stevelosh.com>
2012-04-21 11:52:38 +0100

    derp, add a :main

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject eve "1.0.0"
   :description "The sample code for Steve Losh's BACON 2012 talk."
   :license {:name "MIT/X11"}
+  :main eve.core
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [incanter "1.3.0"]])
